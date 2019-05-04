ac4c5d0854bbd30ca4bc78eba09863696ce3c86b
s-maibuecher <s.maibuecher@gmail.com>
2019-05-04 23:05:34 +0200

    transformed bigger features to several smaller features

--- feature-engineering.py
+++ feature-engineering.py
@@ -1,12 +1,46 @@
 """ Script for Data Preprocessing, especially fields like ingredients which contains big JSON-like Objects: """
 
 import pandas as pd
+import xml.etree.ElementTree as ET
 
 df = pd.read_csv('./CSV/recipe_dataframe.csv', encoding='utf-8', sep='\t')
 
-ingr = df['ingredients'][0]
+new_df = df.copy()
 
-print(ingr)
-# Todo: und weiter geht's
+for index, row in df.iterrows():
+    ingr = '<root>' + df['ingredients'][index] + '</root>'
 
-# ... fetch all ingredients of all recipes and onehotecode these fields to every row...
+    yields_root = ET.fromstring(df['yields'][index])
+
+    root = ET.fromstring(ingr)
+
+    items = root.findall('item') #take the family name to normalize the ingredients
+
+    for item in items:
+        item_name = item.find('./family/name')
+        column_name = f'ingr_{item_name.text}'
+        if not column_name in new_df.columns:
+            new_df[column_name] = None
+
+        item_id = item.find('./id').text
+
+        yield_item = yields_root.find(f'./ingredients/item[id="{item_id}"]')
+
+        try:
+            yield_string = yield_item.find('./amount').text + ' ' + yield_item.find('./unit').text
+        except TypeError:
+            yield_string = None
+
+        new_df[column_name][index] = yield_string
+
+
+
+
+print(new_df.shape)
+
+
+# Todo unwichtige Zutaten wie Salz, Pfeffer werden nicht mitgeliefert, bekommen bei shipped den Wert False
+#
+# Todo xml rezept dateien einchecken
+#
+# Todo im oxygen abchecken, ob alle die gleichen nutritionnames beinhalten
