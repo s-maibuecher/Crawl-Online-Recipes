6447891fc9705121326fc1d7b50853ee89bc255b
s-maibuecher <s.maibuecher@gmail.com>
2019-05-13 22:43:56 +0200

    normalized ingredients

--- feature-engineering.py
+++ feature-engineering.py
@@ -7,36 +7,51 @@ df = pd.read_csv('./CSV/recipe_dataframe.csv', encoding='utf-8', sep='\t')
 
 new_df = df.copy()
 
+new_df['yield_amounts'] = None
+
 for index, row in df.iterrows():
-    ingr = '<root>' + df['ingredients'][index] + '</root>'
+    ingr_tree = '<root>' + df['ingredients'][index] + '</root>'
 
-    try:
-        yields_root = ET.fromstring(df['yields'][index]) # Todo: gibt es hier immer 3 items? dann muss nur ein root element drum rum
-    except ET.ParseError:
-        pass
+    yields_root = ET.fromstring('<root>' + df['yields'][index] + '</root>')
 
-    root = ET.fromstring(ingr)
+    yield_amounts = []
+    yield_list = yields_root.findall('./item/yields')
+
+    for y in yield_list:
+        yield_amounts.append(y.text)
+
+    new_df['yield_amounts'][index] = yield_amounts
+
+    # Todo die meißten Gerichte haben ['2', '3', '4'] yield amounts, siehe Grafik
+    # das hier sind Werte aus den ersten 250 Rezepten
+    # einige haben aber auch ['2', '4', '6', '8']
+    # und eins hat [3]
+    # also müssen die Stückzahlen unten genormt werden
+    # soll ich noch aufnehmen welches Rezept wieviele yields anbietet?
+
+    root = ET.fromstring(ingr_tree)
 
     items = root.findall('item') #take the family name to normalize the ingredients
 
     for item in items:
         item_name = item.find('./family/name')
-        column_name = f'ingr_{item_name.text}'
+        column_name = f'ingr_normalized_{item_name.text}'
         if not column_name in new_df.columns:
             new_df[column_name] = None
 
         item_id = item.find('./id').text
 
-        yield_item = yields_root.find(f'./ingredients/item[id="{item_id}"]')
+        yield_item = yields_root.find(f'./item[1]/ingredients/item[id="{item_id}"]')
+        yield_item_amount = int(yields_root.find(f'./item[1]/yields').text)
 
         try:
-            yield_string = yield_item.find('./amount').text + ' ' + yield_item.find('./unit').text
+            yield_string = yield_item.find('./amount').text + '/' + str(yield_item_amount) + ' ' + yield_item.find('./unit').text
         except TypeError:
             yield_string = None
 
         new_df[column_name][index] = yield_string
 
-
+        # todo test that ;)
 
 
 print(new_df.shape)
