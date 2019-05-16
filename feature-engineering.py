72dbf7ccaec892e8c1230097d4438f94eab5fd7c
Stefan Maibücher <s.maibuecher@gmail.com>
2019-05-16 16:48:44 +0200

    all data extracted

--- feature-engineering.py
+++ feature-engineering.py
@@ -32,16 +32,76 @@ def func_applied_to_category(xml_string):
 
 new_df['category'] = new_df['category'].apply(func_applied_to_category)
 
+def func_applied_to_cuisines(xml_string):
+    ''' function that extracts the values of the cuisines feature: '''
+    if str(xml_string) == 'nan':
+        return None
+    xml_string = '<root>' +xml_string+ '</root>'
+    root = ET.fromstring(xml_string)
+
+    return '#'.join([x.text for x in root.findall('./item/name')])
+
+
+new_df['cuisines'] = new_df['cuisines'].apply(func_applied_to_cuisines)
+
+
+def func_applied_to_label(xml_string):
+    ''' function that extracts the values of the label feature: '''
+    if str(xml_string) == 'nan':
+        return None
+    root = ET.fromstring(xml_string)
+    root_type_attr = root.attrib['type']
+    if root_type_attr == 'null':
+        return None
+    else:
+        return root.find('./text').text
+
+
+new_df['label'] = new_df['label'].apply(func_applied_to_label)
+
+
+# new nutrition columns:
+nutr_col_names = ['Energie (kJ)', 'Energie (kcal)', 'Fett', 'davon gesättigte Fettsäuren', 'Kohlenhydrate', 'davon Zucker', 'Ballaststoffe', 'Eiweiß', 'Cholesterol', 'Salz']
+
+for n in nutr_col_names:
+    new_df['nutrition_' + n] = None
+
+
+new_df['how_many_steps_needed'] = None
+
 for index, row in df.iterrows():
 
     # Todo delete that for production:
     # FOR DEBUGGING
-    if index > 10:
+    if index > 50:
         break
     # END DEBUGGING
 
+    #howmanystepsneeded:
+    steps_tree = '<root>' + df['steps'][index] + '</root>'
+    steps_root = ET.fromstring(steps_tree)
+    all_steps = steps_root.findall('./item/index')
+    steps_needed = max([int(x.text) for x in all_steps])
+    new_df['how_many_steps_needed'][index] = steps_needed if steps_needed > 1 else None # if only one step is in the data, this seems to be a placeholder
+
+
+    # fill nutrition columns:
+    nut_tree = '<root>' + df['nutritionnames2compare'][index] + '</root>'
+    nut_root = ET.fromstring(nut_tree)
+    nut_items = nut_root.findall('./item')
+
+    for item in nut_items:
+        col_name = item.find('./name').text
+        if col_name not in nutr_col_names:
+            raise Exception('neues nutration Feld gefunden? '+ col_name)
+        else:
+            amount = item.find('./amount').text
+            unit = item.find('./unit').text
+            new_df['nutrition_' + col_name][index] = f'{amount} {unit}'
+
     print(f'Neue Zeile: {index}')
 
+    # ingredients calculations:
     ingr_tree = '<root>' + df['ingredients'][index] + '</root>'
 
     yields_root = ET.fromstring('<root>' + df['yields'][index] + '</root>')
@@ -80,6 +140,9 @@ for index, row in df.iterrows():
 
         new_df[column_name][index] = yield_string
 
+    # till here: ingredients calculations
+
+
 
 print('Shape after feature engineering: ' + str(new_df.shape))
 
@@ -94,9 +157,10 @@ new_df.to_csv(os.path.join('CSV', 'final_dataframe.csv'), sep='\t', encoding='ut
 # Todo im oxygen abchecken, ob alle die gleichen nutritionnames beinhalten
 #
 # Todo alte große Spalten rauslöschen
+#
+# Todo nutrition Werte sind pro Portion??
 
-
-# Todo: inspect columns:
+# inspect columns:
 
 # active is always True -> boring
 # author are 17 unique values
@@ -106,4 +170,12 @@ new_df.to_csv(os.path.join('CSV', 'final_dataframe.csv'), sep='\t', encoding='ut
 # clonedFrom is interesting
 # comment just two values, what for?
 # createdAt can be timeplotted
-# cuisines todo hier das extracten wie category
+# cuisines is extraced, if more that one value, than # seperated
+# isDinnerToLunch is always False :)
+# isExcludedFromIndex is always False :)
+# label is extracted
+# name, why is "Marokkanische Rindfleisch-Burger" used 8 times??
+# nutrition extracted
+# steps extracted - are there only none or 6 steps??
+# subtitle only 374 unique values
+# yieldType always servings -> boring
